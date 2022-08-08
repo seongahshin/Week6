55fd003c958c743c0c966b0ee96a04d96ecb32dd
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-08-08 09:27:15 +0900

    Initial Commit

---
+++ Week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week6
+//
+//  Created by 신승아 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
